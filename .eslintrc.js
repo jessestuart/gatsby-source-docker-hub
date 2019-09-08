6cb7643b4cd95c88854ae5672278237f7395ed92
Jesse Stuart <hi@jessestuart.com>
2019-09-08 12:53:10 -0400

    fix(npm): Don't include unused type declarations.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:import/typescript',
-    'plugin:promise/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
@@ -14,6 +13,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   env: {
+    es6: true,
     node: true,
   },
   rules: {
